9366c08fca20061586ba06e1288c278b831fef1c
Anubhav Maheshwari <55053867+anubhavmaheshwari0805@users.noreply.github.com>
2020-03-28 16:04:26 +0530

    Add files via upload

--- PetersonSynch.c
+++ PetersonSynch.c
@@ -1,13 +1,36 @@
+/*Problem:The producer consumer problem (or bounded buffer problem) describes two processes,
+the producer and the consumer, which share a common, fixed-size buffer used as a queue.
+Producer produce an item and put it into buffer.
+If buffer is already full then producer will have to wait for an empty block in buffer.
+Consumer consume an item from buffer.
+If buffer is already empty then consumer will have to wait for an item in buffer.
+Implement Peterson�s Algorithm for the two processes using shared memory such that there is mutual exclusion between them.
+The solution should have free from synchronization problems.*/
 #include<stdio.h>
 #include<pthread.h>
 #include<stdlib.h>
 #include<time.h>
+#include<unistd.h>
 #define max 10
 int flag[2];
-int turn;
-int buffer[max];
+int turn;                                         //turn=1 means producer's turn and turn=0 means consumer's turn.
+int buffer[max];                                  //queue named as buffer which is shared by producer and consumer both.
 int rear=-1;
 clock_t st;
+void displaybuffer()
+{
+	int i;
+	if(rear<0)
+	{
+		printf("Buffer is Empty.\n\n");
+		return;
+	}
+	printf("Buffer : ");
+	for(i=0;i<rear;i++)
+	printf("%d-",buffer[i]);
+	printf("%d\n\n",buffer[i]);
+	return;
+}
 void produce(int item)
 {
 	if(rear==max-1)
@@ -44,41 +67,27 @@ void consume()
 	exit(0);
 	return;
 }
-void displaybuffer()
-{
-	int i;
-	if(rear<0)
-	{
-		printf("Buffer is Empty.\n\n");
-		return;
-	}
-	printf("Buffer : ");
-	for(i=0;i<rear;i++)
-	printf("%d-",buffer[i]);
-	printf("%d\n\n",buffer[i]);
-	return;
-}
 void *producer()
 {
 	do
 	{
-		flag[1]=1;//producer is ready to produce.
-		turn=0; //turn=1 means producer's turn and turn=0 means consumer's turn.
-		while(flag[0]==1&&turn==0);
-		produce(rand()%10);
-		flag[1]=0;
-	}while(turn==1);
+		flag[1]=1;                                 //producer is ready to produce.
+		turn=0;                                    //consumer's turn
+		while(flag[0]==1&&turn==0);                //checking of consumer to consume.
+		produce(rand()%10);                        //producer produces an element.
+		flag[1]=0;                                 //now, producer is not ready to produce.
+	}while(turn==1);                               //when this is producer's turn to produce.
 }
 void *consumer()
 {
 	do
 	{
-		flag[0]=1;
-		turn=1; //turn=1 means producer's turn and turn=0 means consumer's turn.
-		while(flag[1]==1&&turn==1);
-		consume();
-		flag[0]=0;
-	}while(turn==0);
+		flag[0]=1;                                 //consumer is ready to consume.
+		turn=1;                                    //producer's turn
+		while(flag[1]==1&&turn==1);                //checking of producer to produce.
+		consume();                                 //consumer consumes an element.
+		flag[0]=0;                                 //now, consumer is not ready to consume.
+	}while(turn==0);                               //when this is consumer's turn to consume.
 }
 void main()
 {
